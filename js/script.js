a335f0d5f760f4544dbbfeafc38de5fe3729b554
Kaio Garcia <kaiofbgarcia@gmail.com>
2022-10-11 10:20:04 -0300

    type add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const pokemonName = document.querySelector('.pokemon-name');
 const pokemonID = document.querySelector('.pokemon-id');
 const pokemonImg = document.querySelector('.pokemon-img');
 
+// const pokemonType = document.querySelector('.pokemon-type-name');
+const pokemonTypeImg = document.querySelector('.pokemon-type-img');
+
+// const pokemonTypeSec = document.querySelector('.pokemon-type-sec-name');
+// const pokemonTypeImgSec = document.querySelector('.pokemon-type-img-sec');
+
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
 const prev = document.querySelector('.btn-prev');
@@ -26,11 +32,23 @@ const renderPokemon = async (pokemon) => {
   const data = await fetchPokemon(pokemon);
     
   if(data){
+    console.log(data);
     pokemonImg.style.display = 'block';
     pokemonName.innerHTML = data.name;
     pokemonID.innerHTML = data.id;
+
+    //pokemonType.innerHTML = data.types[0].type.name;
+    pokemonTypeImg.src = `./img/icons/${data.types[0].type.name}.ico`;
+    // if(data.types[1]){
+    //   pokemonTypeSec.innerHTML = data.types[1].type.name;
+    //   pokemonTypeImgSec.src = `./img/icons/${data.types[1].type.name}.ico`;
+    // }
+    // pokemonTypeSec.innerHTML = data.types[1].type.name;
+    // pokemonTypeImgSec.src = `./img/icons/${data.types[1].type.name}.ico`;
+    
     pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
+    searchPokemon = data.id;
   } else {
     pokemonImg.style.display = 'none';
     pokemonName.innerHTML = 'Not Found...';
